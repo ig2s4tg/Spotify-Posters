caf86ba1471b1007250f3cdbfdcff9840baabc2c
Walter Sagehorn <w.sagehorn@gmail.com>
2017-09-18 13:46:04 -0500

    updated fetch

--- sp/fetch.py
+++ sp/fetch.py
@@ -2,12 +2,15 @@ import requests
 from urllib.request import urlretrieve as urlr
 
 def get_image_url(album_name, offset=0, size=0):
-    r = requests.get("https://api.spotify.com/v1/search?q=" + album_name + "&type=album")
+    return get_all_image_urls(album_name, size)[offset]
+
+def get_all_image_urls(album_name, size=0):
+    r = requests.get("http://api.spotify.com/v1/search?q=" + album_name + "&type=album")
     r = r.json()
     r = r["albums"]["items"]
     if len(r) == 0:
         print("no albums found for " + album_name)
-    return r[offset]["images"][size]["url"].replace("https:","http:") #lol
+    return list(map(lambda x : x.replace("https:","http:"), [i["images"][size]["url"] for i in r]))
 
 def download_image(image_url, folder="./img/", file_name=""):
     if file_name == "":
@@ -24,4 +27,4 @@ def download_all(input_file="albums.txt", folder="./img/"):
             if line.split(" ")[-1] != "0":
                 offset = int(line.split(" ")[-1])
             url = get_image_url(name,offset,0)
-            download_image(url, file_name=name + ".jpg")
+            download_image(url, folder, file_name=name + ".jpg")
